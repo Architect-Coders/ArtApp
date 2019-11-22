a00239bcddd0e928dc931dc89be95db6a9b7c916
Yeho <jsh085@gmail.com>
2019-11-22 15:28:23 -0600

    se resuelven conflictos del merge

--- app/src/main/java/com/android/leivacourse/artapp/ui/artgallery/ArtGalleryActivity.kt
+++ app/src/main/java/com/android/leivacourse/artapp/ui/artgallery/ArtGalleryActivity.kt
@@ -2,11 +2,6 @@ package com.android.leivacourse.artapp.ui.artgallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import com.android.leivacourse.artapp.R
 
 class ArtGalleryActivity : AppCompatActivity() {
