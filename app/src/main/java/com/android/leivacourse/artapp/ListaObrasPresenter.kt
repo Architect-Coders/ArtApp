65412df33b173e1bb9daee8a10651e136075165c
Yeho <jsh085@gmail.com>
2019-11-21 20:28:08 -0600

    se actualiza el argumento del presenter en listadoObras

--- app/src/main/java/com/android/leivacourse/artapp/ListaObrasPresenter.kt
+++ app/src/main/java/com/android/leivacourse/artapp/ListaObrasPresenter.kt
@@ -1,6 +1,6 @@
 package com.android.leivacourse.artapp
 
-class ListaObrasPresenter(_view: ListaObrasFragment) : ListaObrasContract.Presenter {
+class ListaObrasPresenter(_view: ListaObrasContract.View) : ListaObrasContract.Presenter {
 
     private var mView : ListaObrasContract.View = _view
     private var mModel : ListaObrasContract.Model = ListaObrasModel(this)
