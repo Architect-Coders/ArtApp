83adc8ec8ca2389dc5162f80da734e5e81810a9c
Ivan Parra <parra.ivan93@gmail.com>
2019-12-02 09:48:55 -0600

    Se remueven crashdemo.

--- app/src/main/java/com/android/leivacourse/artapp/ui/artgallery/ArtGalleryActivity.kt
+++ app/src/main/java/com/android/leivacourse/artapp/ui/artgallery/ArtGalleryActivity.kt
@@ -2,27 +2,14 @@ package com.android.leivacourse.artapp.ui.artgallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
 import com.android.leivacourse.artapp.R
-import com.crashlytics.android.Crashlytics
+
 
 class ArtGalleryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val crashButton = Button(this)
-        crashButton.text = "Crash!"
-        crashButton.setOnClickListener {
-            Crashlytics.getInstance().crash() // Force a crash
-        }
-
-        addContentView(crashButton, ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT))
-
     }
 
 }
