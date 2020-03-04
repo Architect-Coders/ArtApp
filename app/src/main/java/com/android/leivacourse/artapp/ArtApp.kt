2aa3b0baa59715337bb2de1f6f6f8c6d27b688aa
selenatorres <sele.torres95@gmail.com>
2020-03-04 15:34:07 -0600

    - Create DB using Room.

--- app/src/main/java/com/android/leivacourse/artapp/ArtApp.kt
+++ app/src/main/java/com/android/leivacourse/artapp/ArtApp.kt
@@ -1,12 +1,23 @@
 package com.android.leivacourse.artapp
 
 import android.app.Application
+import androidx.room.Room
+import com.android.leivacourse.artapp.data.local.ArtsDatabase
 
 
 class ArtApp: Application() {
 
+    lateinit var db: ArtsDatabase
+    private set
+
     override fun onCreate() {
         super.onCreate()
         initDI()
+
+        db = Room.databaseBuilder(
+            this,
+            ArtsDatabase::class.java,
+            "arts-db"
+        ).build()
     }
 }
