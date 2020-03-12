e17162595f250bf5453b693858efddc2da9f8a05
Yeho <jsh085@gmail.com>
2020-03-12 14:10:14 -0600

    se elimina codigo innecesario

--- app/src/main/java/com/android/leivacourse/artapp/ArtApp.kt
+++ app/src/main/java/com/android/leivacourse/artapp/ArtApp.kt
@@ -1,24 +1,11 @@
 package com.android.leivacourse.artapp
 
 import android.app.Application
-import androidx.room.Room
-import com.android.leivacourse.artapp.data.local.ArtsDatabase
 
-
-class ArtApp: Application() {
-
-    lateinit var db: ArtsDatabase
-    private set
+class ArtApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         initDI()
-
-        db = Room.databaseBuilder(
-            this,
-            ArtsDatabase::class.java,
-            "arts-db"
-        ).fallbackToDestructiveMigration()
-         .build()
     }
 }
