c1850c388c489581b0f2d17ae96ae47fd333b342
Ivan Parra <parra.ivan93@gmail.com>
2020-03-13 17:24:58 -0600

    Modification in favorite design for detail view

--- app/src/main/java/com/android/leivacourse/artapp/ui/detail/DetailArtActivity.kt
+++ app/src/main/java/com/android/leivacourse/artapp/ui/detail/DetailArtActivity.kt
@@ -1,5 +1,8 @@
 package com.android.leivacourse.artapp.ui.detail
 
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,11 +13,9 @@ import com.android.leivacourse.artapp.ui.camera.CameraArtActivity
 import com.android.leivacourse.artapp.R
 import com.android.leivacourse.artapp.data.local.model.ArtDetail
 import com.android.leivacourse.artapp.ui.detail.DetailArtViewModel.UiModel
-import com.android.leivacourse.artapp.utils.app
 import com.android.leivacourse.artapp.utils.loadUrl
 import com.android.leivacourse.artapp.utils.myStartActivity
 import kotlinx.android.synthetic.main.activity_detail_art.*
-import kotlinx.android.synthetic.main.item_artwork.*
 import org.koin.androidx.scope.currentScope
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
@@ -53,7 +54,8 @@ class DetailArtActivity : AppCompatActivity(){
         btnPreview.setOnClickListener {
             viewModel.onPreviewPushed(art)
         }
-        btn_favoritex.setOnClickListener {
+
+        btn_favorite.setOnClickListener {
             viewModel.favMenuSelected()
         }
     }
@@ -73,9 +75,20 @@ class DetailArtActivity : AppCompatActivity(){
         }
 
     private fun updateUI(model: UiModel) = with (model.art){
-            val icon = if (favorite) R.drawable.ic_favorite_pushed else R.drawable.ic_favorite
-            btn_favoritex?.setImageDrawable(getDrawable(icon))
-            photoDetailToolbar.title = title
+        val icon = if (favorite) R.drawable.ic_favorite_pushed else R.drawable.ic_fav
+
+        if(favorite) {
+            val myFabSrc = resources.getDrawable(icon,null)
+            val willBeRed: Drawable = myFabSrc.getConstantState()!!.newDrawable()
+
+           willBeRed.mutate().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY)
+
+            btn_favorite.setImageDrawable(willBeRed)
+        }else {
+            btn_favorite?.setImageResource(icon);
+        }
+
+        photoDetailToolbar.title = title
             photoDetailImage.loadUrl("${urls?.regular}")
             photoSummary.setArt(model.art)
             photoUser.loadUrl("${user?.profileImage?.small}")
